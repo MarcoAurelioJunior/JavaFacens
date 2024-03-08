50b11723b33519623e3b1c517f228f1226544dba
MarcoAurelioJunior <marcoaurelioroquedeoliveirajun@gmail.com>
2024-03-08 11:24:08 -0300

    update: New file (Operators)

---
+++ javaIndex/src/Operadores/operatorsNote.java
@@ -0,0 +1,56 @@
+package Operadores;
+
+public class operatorsNote {
+    public static void main(String[] args) {
+        // Operators Type
+        
+        /* Aritiméticos
+         * Relacionais
+         * Atribuição
+         * Lógicos
+        */
+
+        //Aritiméticos
+
+            /*
+             * Adição (+)
+             * Subtração (-)
+             * Multiplicação (*)
+             * Divisão (/)
+             * Módulo (%) // Utilizado para saber o resto da divisão
+             * Incremento (++)
+             * Decremento (--)
+             * OBS: concatenação // System.out.println("A soma é: " + soma);
+            */
+
+        //Relacionais (Return True or False)
+
+            /*
+             * >: Maior que
+             * <: Menor que
+             * >=: Maior ou igual que
+             * <=: Menor ou igual que 
+             * ==: Igual a
+             * !=: Diferente de 
+             */
+        
+        //Atribuição
+
+            /*
+             * =: Atribuição
+             * +=: Soma
+             * -=: Subtração
+             * *=: Multiplicação
+             * /=: Divisão
+             * %=: Módulo
+             */
+
+        //Lógicos
+
+            /*
+             * &&: E/AND
+             * ||: OU/OR
+             * !: Negação/NOT
+             */
+    }
+}
