e85ce45394cb4c23e9ebb219665d9e92c5dea590
MarcoAurelioJunior <marcoaurelioroquedeoliveirajun@gmail.com>
2024-04-11 22:01:47 -0300

    UPDATE

--- Semestre1/While/calculadoComSwitch.java
+++ Semestre1/While/calculadoComSwitch.java
@@ -6,47 +6,71 @@ public class calculadoComSwitch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        do {
-            
+        int opcao = 1;
+        int num1 = 0;
+        int num2 = 0;
+
+        while (opcao != 5 ) {
             System.out.println("\nO que deseja fazer?\n");
             System.out.print("1 - Somar \n2 - Subtrair \n3 - Multiplicar \n4 - Dividir \n5 - Sair \nEscolha uma dessas opções: ");
-            int opcao = scanner.nextInt();
+            opcao = scanner.nextInt();
 
-            System.out.print("Escreva um número: ");
-            int num1 = scanner.nextInt();
-
-            System.out.print("Escreva outro número: ");
-            int num2 = scanner.nextInt();
-        
+            
             switch (opcao) {
                 case 1:
-                int soma = num1 + num2;
+                    System.out.print("Escreva um número: ");
+                    num1 = scanner.nextInt();
+
+                    System.out.print("Escreva outro número: ");
+                    num2 = scanner.nextInt();
+
+                    int soma = num1 + num2;
                     System.out.println("\nAo somar " + num1 + " e " + num2  + " temos o resultado de: " + soma);
                     break;
                     
                 case 2:
+                    System.out.print("Escreva um número: ");
+                    num1 = scanner.nextInt();
+
+                    System.out.print("Escreva outro número: ");
+                    num2 = scanner.nextInt();
+
+
                     int subtrair = num1 - num2;
                     System.out.println("\nAo subtrair " + num1 + " e " + num2 + " temos o resultado de: " + subtrair);
                     break;
 
                 case 3:
+                    System.out.print("Escreva um número: ");
+                    num1 = scanner.nextInt();
+
+                    System.out.print("Escreva outro número: ");
+                    num2 = scanner.nextInt();
+
                     int multiplicar = num1 * num2;
                     System.out.println("\nMultiplicando " + num1 + " e " + num2 + " temos o resultado de: " + multiplicar);
                     break;
 
                 case 4:
+                    System.out.print("Escreva um número: ");
+                    num1 = scanner.nextInt();
+
+                    System.out.print("Escreva outro número: ");
+                    num2 = scanner.nextInt();
+                    
                     int dividir = num1 / num2;
                     System.out.println("\nAo dividir " + num1 + " e " + num2 + " temos o resultado de: " + dividir);
                     break;
-
-                case 5:
-                    System.out.println("Programa encerrado.");
-                    break;
-
                 default:
-                    System.out.println("Opa, não temos essa opção, tente novamente!");
+                    if (opcao == 5) {
+                        System.out.println("Programa encerrado.");
+                    }else{
+                        System.out.println("Opção invalida, tente novamente!");
+                    }
                     break;
             }
-        } while (true);
+    
+
+        }
     }
 }
