4d8d539fd2511d49bcbf36afdf4a0942e7a553f8
MarcoAurelioJunior <marcoaurelioroquedeoliveirajun@gmail.com>
2024-05-15 20:25:41 -0300

    update

--- Semestre1/Vetor/exercicio8.java
+++ Semestre1/Vetor/exercicio8.java
@@ -5,29 +5,28 @@ import java.util.Scanner;
 public class exercicio8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         int[] vetorRemove = new int[5];
 
         for(int i = 0; i < vetorRemove.length; i++){
-            System.out.print("Digite o elemento " + (i + 1) + ": ");
+            System.out.print("Digite o elemento " + i + ": ");
             vetorRemove[i] = scanner.nextInt();
         }
-
-        System.out.print("Digite o número que você deseja tirar: ");
-        int retira = scanner.nextInt();
+        
+        System.out.println("Digite o valor a ser retirado: ");
+        int valorRetiado = scanner.nextInt();
 
         int contador = 0;
-
-        for (int j = 0; j < 5; j++) {
-            if (vetorRemove[j] != retira) { //Se algum elemento for diferente do número que quer retirar então...
-                vetorRemove[contador++] = vetorRemove[j]; //No indice 1 da lista, adicione o elemento que for diferente
-            } // Se o elemento for igual ao que quer retirar então ele apenas ignora, e coloca +1 no contador que pula um elemento
+        for(int j = 0; j <  vetorRemove.length; j++){
+            if(vetorRemove[j] != valorRetiado){
+                vetorRemove[contador++] = vetorRemove[j];
+            }
         }
 
-        for (int k = 0; k < contador; k++) { //Coloca o contador para que fique do tamanho da lista atual
-            System.out.println(vetorRemove[k] + ", " );
+        for(int k = 0; k < contador; k++){
+            System.out.print(vetorRemove[k] + " ");
         }
 
-        scanner.close();        
+        scanner.close();
     }
 }
