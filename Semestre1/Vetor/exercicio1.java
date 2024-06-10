74576279033d50b106d99465fe736043fb04ffb1
MarcoAurelioJunior <marcoaurelioroquedeoliveirajun@gmail.com>
2024-06-09 22:20:51 -0300

    update

--- Semestre1/Vetor/exercicio1.java
+++ Semestre1/Vetor/exercicio1.java
@@ -2,7 +2,7 @@ package Semestre1.Vetor;
 
 public class exercicio1 {
     public static void main(String[] args) {
-        int[] vetor = {2, 5, 7, 10, 20};
+        int[] vetor = {2, 6, 7, 10, 20};
 
         for(int i = 0; i < vetor.length; i++){
             System.out.print(vetor[i] + " - ");
